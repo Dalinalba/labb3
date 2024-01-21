d5aa64b57ff888e456fc0ef91a9c9c5aa0cdb529
Dalinalba <148215916+Dalinalba@users.noreply.github.com>
2024-01-21 13:28:15 +0100

    change name

--- nextjs-blog/pages/country/[country].test.js
+++ nextjs-blog/pages/country/[country].test.js
@@ -1,23 +1,21 @@
 // pages/country/[country].test.js
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Country from '../../pages/country/[country]';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Country from './[country]'
 
 describe('Country Page', () => {
-  it('renders the component', () => {
-    const mockProps = {
-      newsData: [
-        { title: 'News 1', description: 'Description 1' },
-        { title: 'News 2', description: 'Description 2' },
-      ],
-      country: 'us',
-    };
+    it('renders the component', () => {
+        const mockProps = {
+            newsData: [
+                { title: 'News 1', description: 'Description 1' },
+                { title: 'News 2', description: 'Description 2' }
+            ],
+            country: 'us'
+        }
 
-    render(<Country {...mockProps} />);
+        render(<Country {...mockProps} />)
 
-    // Check if the component renders correctly
-    expect(screen.getByText('Top Headlines in us')).toBeInTheDocument();
-
-    // Add more specific tests based on your component's content and structure
-  });
-});
+        // Check if the component renders correctly
+        expect(screen.getByText('Top Headlines in us')).toBeInTheDocument()
+    })
+})
